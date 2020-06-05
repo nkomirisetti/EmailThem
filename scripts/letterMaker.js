8dd203be097a17bde23c119510dec2892b07cb36
Nikhil Komirisetti <nikhilk@live.unc.edu>
2020-06-05 00:49:57 -0400

    fix

--- scripts/letterMaker.js
+++ scripts/letterMaker.js
@@ -1,17 +1,17 @@
-function makeLetter(userName, official) {
-return `Dear ${official.name},
-\r\n\r\n %0D%0A %0D%0A
-I'm writing you today to address an issue that affects all of us. For far too long, we have done too little to address systemic inequalities in our nation. The problem is widespread, and affects everything from employment and affordable housing, to education and safety. As your constituent, I want you to know that these are issues very dear to me, and will directly influence which officials I support.
-\r\n\r\n %0D%0A %0D%0A
-As the ${official.office}, you have far more power than I do, which is why I write this email to you. I believe that you can introduce changes and impact our community in a way that will help us address these systemic inequalities. I, like yourself, want only the best for our constituency, and I believe that the best course of action will be to actively fight against these inequalities.
-\r\n\r\n %0D%0A %0D%0A
-When lives are at stake, inaction is unacceptable. I appreciate you taking the time to read this email, and I hope it influences you to consider making a tangible difference.
-\n\n %0D%0A %0D%0A
-Sincerely,
+// function makeLetter(userName, official) {
+// return `Dear ${official.name},
+// \r\n\r\n %0D%0A %0D%0A
+// I'm writing you today to address an issue that affects all of us. For far too long, we have done too little to address systemic inequalities in our nation. The problem is widespread, and affects everything from employment and affordable housing, to education and safety. As your constituent, I want you to know that these are issues very dear to me, and will directly influence which officials I support.
+// \r\n\r\n %0D%0A %0D%0A
+// As the ${official.office}, you have far more power than I do, which is why I write this email to you. I believe that you can introduce changes and impact our community in a way that will help us address these systemic inequalities. I, like yourself, want only the best for our constituency, and I believe that the best course of action will be to actively fight against these inequalities.
+// \r\n\r\n %0D%0A %0D%0A
+// When lives are at stake, inaction is unacceptable. I appreciate you taking the time to read this email, and I hope it influences you to consider making a tangible difference.
+// \n\n %0D%0A %0D%0A
+// Sincerely,
 
-%0D%0A %0D%0A
-`
-}
+// %0D%0A %0D%0A
+// `
+// }
 
 function makeLetter(userName, official, city, state) {
 `Dear ${official.name},
